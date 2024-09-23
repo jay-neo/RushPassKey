b8546cfa96a7cdf247be11c30422fefde68adfa9
Jagadish Sau <jsau5337@gmail.com>
2024-09-23 23:55:03 +0530

    small edit for prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from 'tailwindcss'
+import tailwindcss from "tailwindcss";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
